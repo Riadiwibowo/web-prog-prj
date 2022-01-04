b9118f0b196949bb95ffc317500c146571b7ae92
Riadiwibowo <riadiwibowo777@gmail.com>
2022-01-05 01:43:10 +0700

    tambah admin, middleware, pokoknya banyak deh

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,10 +1,22 @@
 <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello, world!</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -26,43 +38,89 @@
             margin-top: auto;
         }
         
-      </style>
-    
-  </head>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    </style>
+
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="/ ">Project-Store</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-      <div   class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="/viewFurniture">View</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link " href="/login">Login</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link " href="/register">Register</a>
-          </li>
-      </ul>
-      </div>
-  </div>
-  </nav>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">tolS</a>
+                </li>    
+            </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
+                
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/viewFurniture">View</a>
+                </li>
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
 
-    <body>
-        <div class="container mt-4" >
-        @yield('container')
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                </li>        
+                <li class="nav-item">
+                    <a class="nav-link" href="/viewFurniture">View</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/home">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/home">Cart</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+            
+            </ul>
         </div>
-    </body>
-    <footer class="footer-area footer--light bg-primary d-flex justify-content-center text-white flex-shrink-0">
-        <p>
-            Place sticky footer content here.
-        </p>
-      </footer>
+    </div>
+</nav>
+
+<body>
+    <div class="container mt-4" >
+        @yield('container')
+    </div>
+</body>
+<footer class="footer-area footer--light bg-primary d-flex justify-content-center text-white flex-shrink-0">
+    <p>
+        Place sticky footer content here.
+    </p>
+</footer>
 </html>
 
