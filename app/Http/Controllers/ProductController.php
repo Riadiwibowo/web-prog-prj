022686dd15116d6f706b2c3ea7237125783a747f
Riadiwibowo <riadiwibowo777@gmail.com>
2022-01-10 15:44:51 +0700

    tambah user and model controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rules\Unique;
 
 class ProductController extends Controller
